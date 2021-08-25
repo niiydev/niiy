560bb6c54cb6226f549709376e20c27d7b9b4005
Torlakee <80884129+torlakee@users.noreply.github.com>
2021-08-25 13:06:35 +0300

    Work on manager gui

---
+++ manager/gui/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div id="app" className="App">
+      <div className="container">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- manager/gui/go.mod
+++ manager/gui/go.mod
@@ -1,3 +1,5 @@
-module niiy-manager-gui
+module niiy-manager
 
 go 1.16
+
+require github.com/wailsapp/wails v1.16.6
